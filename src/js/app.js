5d9319b89cebb01099cc0239eeb483ccfcdda69d
shahrizoda <shahrizodadeveloping@gmail.com>
2022-07-21 14:59:45 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,16 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+
+const checkHeader = () => {
+    const header = document.querySelector('.header');
+    if(window.scrollY > 0){
+        header.classList.add('active')
+    }else{
+        header.classList.remove('active')
+    }
+}
+
+document.addEventListener('scroll', checkHeader)
+document.addEventListener('load', checkHeader)
